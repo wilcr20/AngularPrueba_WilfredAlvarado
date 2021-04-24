01353d8eea9582969cdbb3075f3f6934ce3eabbb
wilcr20 <wilcr20@estudiantec.cr>
2021-04-23 18:41:27 -0600

    4 files modified, 1 added

---
+++ src/app/Modelos/Person.ts
@@ -0,0 +1,4 @@
+export class Person {
+  Name: string;
+  LastName: string;
+}
--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -1,10 +1,34 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input } from "@angular/core";
+import { Person } from "./Modelos/Person";
 
 @Component({
-  selector: 'hello',
-  template: `<h1>Hello {{name}}!</h1>`,
-  styles: [`h1 { font-family: Lato; }`]
+  selector: "hello",
+  template: `
+    <h1>Hello {{ name }}!</h1>
+  `,
+  styles: [
+    `
+      h1 {
+        font-family: Lato;
+      }
+    `
+  ]
 })
-export class HelloComponent  {
+export class HelloComponent {
   @Input() name: string;
+
+  listPerson: Person[] = [
+    {
+      Name: "Sandra",
+      LastName: "Lopez"
+    },
+    {
+      Name: "Andrea",
+      LastName: "Solis"
+    },
+    {
+      Name: "Roberto",
+      LastName: "Sanchez"
+    }
+  ];
 }
